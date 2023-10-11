ab06ffdb7402625c1abc176e8a6e0a95b054855f
Adventureandre <adventureandre@hotmail.com>
2023-10-10 22:55:49 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
         Department obj =  new Department(1,"andre");
 
         Seller seller =  new Seller(1,"andre","andre@email", new Date(),3000.0,obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
     }
