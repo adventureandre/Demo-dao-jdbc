898583156476f51db57abd705210d1769f5e5aad
Andre Luiz <adventureandre@hotmail.com>
2023-10-10 21:59:09 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department obj =  new Department(1,"andre");
 
+        Seller seller =  new Seller(1,"andre","andre@email", new Date(),3000.0,obj);
 
-        System.out.println(obj.toString());
-        System.out.println( obj.hashCode());
-
+        System.out.println(seller);
     }
 }
