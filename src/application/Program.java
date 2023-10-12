f16c32ae940069774512f9148b122b8e96ae8506
Adventureandre <adventureandre@hotmail.com>
2023-10-12 20:24:53 -0300

    Reusing intantiantion

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,7 +12,8 @@ public class Program {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        Seller seller = sellerDao.findById(2);
+        System.out.println("=== Test 1: seller findByID ===");
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
