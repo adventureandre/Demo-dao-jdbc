78b530b60c33c0b1393cb993c15d5e2ea87c4392
Adventureandre <adventureandre@hotmail.com>
2023-10-14 23:35:17 -0300

    Delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,9 +7,11 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -45,5 +47,13 @@ public class Program {
         sellerDao.uddate(seller);
         System.out.println("Update Completed");
 
+
+        System.out.println("\n === Test 6: seller Delete ===");
+        System.out.println("Enter id for delete Test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
+
+        sc.close();
     }
 }
